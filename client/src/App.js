370ba8d1559c13323048dab3ceca194dff2a531e
jheckel <jalen.heckel@icloud.com>
2021-11-20 14:21:34 -0800

    got SignIn imported need to either put it in NavBar or render on 'login' click

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import { Profile } from "./pages/Profile";
 import { SearchPhotos } from "./pages/SearchPhotos";
 import { Upload } from "./pages/Upload";
 
+
 import {Navbar} from "./components/Navbar"
 
 const httpLink = createHttpLink({
