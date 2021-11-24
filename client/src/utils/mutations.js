67d58602b096009492c184f3d132f09f3f9fcd24
Ilya Spivakov <xaker00@gmail.com>
2021-11-23 21:53:14 -0800

    login form and mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,16 @@
 import { gql } from "@apollo/client";
 
-// export const LOGIN_USER = gql``;
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
 // export const ADD_USER = gql``;
 
