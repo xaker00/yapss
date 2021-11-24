220b062af185496c31f499ccb938a512653d5d39
Ilya Spivakov <xaker00@gmail.com>
2021-11-23 22:00:59 -0800

    proper comment in graphql

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const GET_PHOTO = gql`
-// Needs testing, does photoId need to be changed to _id?
+  # Needs testing, does photoId need to be changed to _id?
   query photo($photoId: String) {
     photo (_id: $photoId) {
       _id
