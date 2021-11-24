ca170b4226007f424795337eb45df085ba122536
Ilya Spivakov <xaker00@gmail.com>
2021-11-23 19:37:15 -0800

    photo upload page

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -1,5 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Paper, TextField, Box, Button } from "@mui/material";
+
+import { ADD_PHOTO } from "../utils/mutations";
+
+import { Hashtags } from "./../components/Hashtags";
+import "./upload.css";
+
+// docs
+// https://www.apollographql.com/blog/graphql/file-uploads/with-react-hooks-typescript-amazon-s3-tutorial/
 
 export const Upload = (props) => {
-  return <div>Upload</div>;
+  // keep track of form data
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    hashtags: [],
+    file: {},
+  });
+
+  // expose graphql as a function
+  const [savePhoto, { error }] = useMutation(ADD_PHOTO);
+
+  // Handle file upload changes
+  const onFileChangeHandlerOld = (event) => {
+    const file = event.target.files[0].file;
+    console.log(event.target);
+    setFormData({ ...formData, file });
+  };
+
+    // Handle file upload changes
+    const onFileChangeHandler = (file) => {
+      console.log(file);
+      setFormData({ ...formData, file });
+    };
+
+  // handle text input changes
+  const onInputChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // upload the photo
+  const submitForm = async () => {
+    const response = await savePhoto({ variables: { ...formData } });
+    console.log("response", response);
+
+    if (error) {
+      throw new Error("something went wrong!");
+    }
+  };
+
+  return (
+    <Paper sx={{ marginTop: 2 }}>
+      <div style={{ width: "100%" }}>
+        <Box sx={{ display: "grid", gridTemplateRows: "repeat(3, 1fr)" }}>
+          <TextField
+            sx={{ m: 2 }}
+            id="title"
+            name="title"
+            label="Title"
+            variant="standard"
+            onChange={onInputChangeHandler}
+            value={formData.title}
+          />
+          <TextField
+            sx={{ m: 2 }}
+            id="description"
+            name="description"
+            label="Description"
+            variant="standard"
+            onChange={onInputChangeHandler}
+            value={formData.description}
+          />
+          <Box sx={{ m: 2 }}>
+            <Hashtags />
+          </Box>
+        </Box>
+      </div>
+      <Box sx={{ m: 2 }} className="files color">
+        <input
+          type="file"
+          name="file"
+          onChange={({
+            target: {
+              validity,
+              files: [file],
+            },
+          }) => validity.valid && onFileChangeHandler(file)}
+        />
+      </Box>
+      <Box sx={{ m: 2 }}>
+        <Button sx={{ m: 2 }} variant="contained" onClick={submitForm}>
+          Upload
+        </Button>
+      </Box>
+    </Paper>
+  );
 };
